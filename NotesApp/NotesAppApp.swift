6672342ebdc81425961d98436310f19d77b7fc57
Dossanbayev Roma <rahmandosanbayev@gmail.com>
2026-06-12 16:01:47 +0500

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Rakhat Dossanbayev on 12.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
