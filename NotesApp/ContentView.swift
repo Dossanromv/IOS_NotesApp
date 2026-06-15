6e03a1f2a36c546c7672b44798a4f35dd3933a6e
Dossanbayev Roma <rahmandosanbayev@gmail.com>
2026-06-15 18:31:01 +0500

    clean

--- NotesApp/ContentView.swift
+++ NotesApp/ContentView.swift
@@ -7,39 +7,18 @@
 
 import SwiftUI
 
-
-struct Note: Identifiable {
-    let id = UUID()
-    var title: String
-    var text: String
-}
-
 struct ContentView: View {
-    
-    
-@State private var notes: [Note] = []
-    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(notes) { note in
-                    Text(note.title)
-                }
-            }
-            .navigationTitle("Notes")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        notes.append(Note(title: "Новая заметка", text: ""))
-                    } label: {
-                        Image(systemName: "plus")
-                    }
-                }
-            }
+        VStack {
+            
         }
         .padding()
     }
+        .
 }
+
+
+
 
 #Preview {
     ContentView()
